2be911c86754ef90bd480e232ce964c0dd12ce68
Rob-Martinez <86384534+Rob-Martinez@users.noreply.github.com>
2022-06-19 13:23:13 -0500

    added files card, cardlist, and robots

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id: 1,
+        name: "James Franklin",
+        username: "JFrank",
+        email: "j.frank281@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Patrick Meirion",
+        username: "PattyM",
+        email: "pattyM@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Trueman Monet",
+        username: "TruMon",
+        email: "truthhurts@hotmail.com"
+    },
+    {
+        id: 4,
+        name: "Judy Rath",
+        username: "Judy12",
+        email: "judyrath32323@hotmail.com"
+    },
+    {
+        id: 5,
+        name: "Ronnie Everitt",
+        username: "RonE",
+        email: "rone99@outlook.com"
+    },
+    {
+        id: 6,
+        name: "Emmy Stone",
+        username: "MStone",
+        email: "Estone3@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Roger Gibbins",
+        username: "Gibbmister",
+        email: "gibby@gmail.com"
+    },
+    {
+        id: 8,
+        name: "Jimmy Goodman",
+        username: "Goody",
+        email:"goodyman@hotmail.com"
+    },
+    {
+        id: 9,
+        name: "Immit Iverson",
+        username: "Ivy2010",
+        email: "immit.ivy@outlook.com"
+    },
+    {
+        id: 10,
+        name: "Billy Tomas",
+        username: "BLT",
+        email: "billyblt@gmail.com"
+    }
+
+]
